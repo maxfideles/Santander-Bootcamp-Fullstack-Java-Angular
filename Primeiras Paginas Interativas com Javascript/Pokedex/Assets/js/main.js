db86710202c07b448fa1f6ee2a98b6bb05e91b9d
maxfideles <maxfideles24@gmail.com>
2023-09-11 12:36:58 -0300

    Convertendo o modelo do PokeApi para nosso objeto

--- Primeiras Paginas Interativas com Javascript/Pokedex/Assets/js/main.js
+++ Primeiras Paginas Interativas com Javascript/Pokedex/Assets/js/main.js
@@ -1,21 +1,17 @@
 
-function convertPokemonToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 
 function arrayToHtml (pokemon){
     return `
-        <li class="pokemon">
-                <span class="number"> #${pokemon.order} </span>
+        <li class="pokemon ${pokemon.type}">
+                <span class="number"> #${pokemon.number} </span>
                 <span class="name">${pokemon.name} </span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${convertPokemonToLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
 
-                    <img src=${pokemon.sprites.other.dream_world.front_default} alt="${pokemon.name}">
+                    <img src=${pokemon.picture} alt="${pokemon.name}">
                 </div>
                 
             </li>
