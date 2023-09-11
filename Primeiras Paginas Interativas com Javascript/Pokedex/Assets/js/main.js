10ada93743f954f4da0f9b0e9d5cd73a14a99beb
maxfideles <maxfideles24@gmail.com>
2023-09-10 22:50:39 -0300

    Transformando Array da Requisicao em Lista html

--- Primeiras Paginas Interativas com Javascript/Pokedex/Assets/js/main.js
+++ Primeiras Paginas Interativas com Javascript/Pokedex/Assets/js/main.js
@@ -4,9 +4,40 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+
+function arrayToHtml (pokemon){
+    return `
+        <li class="pokemon">
+                <span class="number"> #001 </span>
+                <span class="name">${pokemon.name} </span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+                
+            </li>
+    `
+}
+
+const pokemonOl = document.getElementById("pokemonList")
+
+
 //it returns a Promisse. It means, it's an assynchronus process
 fetch(url)
     //not using arrow function: .then(function(response){return response.json})
     .then((response) =>response.json())
-    .then((jsonBody) => jsonBody)
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            pokemonOl.innerHTML += arrayToHtml(pokemon)
+        }
+
+    } )
     .catch((error) => console.error(error))
