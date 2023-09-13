0549339bec19e198c2b14cf0cd2f8ef4661f3cad
maxfideles <maxfideles24@gmail.com>
2023-09-13 00:21:47 -0300

    Criando o Style do Component

--- Ganhando Produtividade com Angular/Trabalhando com Web Components no Front-end/src/components/CardNews.js
+++ Ganhando Produtividade com Angular/Trabalhando com Web Components no Front-end/src/components/CardNews.js
@@ -1,4 +1,5 @@
  class CardNews extends HTMLElement{
+   
     constructor(){
         super()
         const shadow = this.attachShadow({mode:"open"})
@@ -12,7 +13,7 @@
         componentRoot.setAttribute("class","card")
 
         const cardLeft = document.createElement("div")
-        cardLeft.setAttribute("class","card__left")
+        cardLeft.setAttribute("class","cardLeft")
 
         const author = document.createElement("span")
         author.textContent = "By " + (this.getAttribute("author") || "Anonymous")
@@ -29,7 +30,7 @@
         cardLeft.appendChild(newsContent)
         
         const cardRight  = document.createElement("div")
-        cardRight.setAttribute("class","card__right")
+        cardRight.setAttribute("class","cardRight")
 
         const imgNews = document.createElement("img")
         imgNews.src = this.getAttribute("photo") || "https://media.istockphoto.com/id/1327592449/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=170667a&w=0&k=20&c=qDvsvfQdmm_cvI_BQH4PdIt8-P-VDAq7ufOobicPBu0="
@@ -45,7 +46,42 @@
     }
 
     styles(){
+        const style = document.createElement("style")
+        style.textContent = `
+            .card{
+                width: 80%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                -webkit-box-shadow: 4px 8px 11px 2px rgba(0,0,0,0.42); 
+                box-shadow: 4px 8px 11px 2px rgba(0,0,0,0.42);
+            }
 
+            .cardLeft{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+            .cardLeft span{
+                font-weight: 500;
+            }
+
+            .cardLeft a{
+                margin-top: 15px;
+                font-size: 30px;
+                color: black;
+                text-decoration: none;
+            }
+
+            .cardLeft p{
+                color: gray;
+            }             
+        `
+
+
+
+        return style
     }
 
  }
