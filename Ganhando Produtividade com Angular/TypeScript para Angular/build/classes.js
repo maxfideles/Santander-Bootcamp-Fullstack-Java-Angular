1340184a68d6c66c8654a9f604f153638da0c20e
maxfideles <maxfideles24@gmail.com>
2023-09-13 16:24:57 -0300

    Trabalhando com Classes

---
+++ Ganhando Produtividade com Angular/TypeScript para Angular/build/classes.js
@@ -0,0 +1,13 @@
+"use strict";
+class Character {
+    constructor(name, strength, skill) {
+        this.name = name;
+        this.strength = strength;
+        this.skill = skill;
+    }
+    attack() {
+        console.log(`Attack with ${this.strength} points`);
+    }
+}
+const p1 = new Character("Ken", 20, 1);
+p1.attack();
---
+++ Ganhando Produtividade com Angular/TypeScript para Angular/src/classes.ts
@@ -0,0 +1,20 @@
+ class Character {
+    "name": string
+    "strength": number
+    "skill": number
+ 
+    constructor(name:string,strength:number,skill:number){
+        this.name = name
+        this.strength = strength
+        this.skill = skill
+    }
+
+    attack(): void{
+         console.log(`Attack with ${this.strength} points`)
+    }
+ }
+
+
+ const p1 = new Character("Ken",20,1)
+
+ p1.attack()
