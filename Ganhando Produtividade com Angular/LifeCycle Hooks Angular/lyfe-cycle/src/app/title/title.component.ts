72db305ff62071c9d69a4ba4dfc9ea1a60316bec
maxfideles <maxfideles24@gmail.com>
2023-09-14 20:15:42 -0300

    OnInit

---
+++ Ganhando Produtividade com Angular/LifeCycle Hooks Angular/lyfe-cycle/src/app/title/title.component.ts
@@ -0,0 +1,19 @@
+ import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+ name: string = "Max"
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.name = `Seja bem-vindo ${this.name}`
+
+  }
+
+}
