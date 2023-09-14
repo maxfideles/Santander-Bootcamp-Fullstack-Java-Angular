30a53256ebd63da49ced7bc82eddec6581f9035a
maxfideles <maxfideles24@gmail.com>
2023-09-14 20:38:12 -0300

    OnChanges LifeCycle Hook

--- Ganhando Produtividade com Angular/LifeCycle Hooks Angular/lyfe-cycle/src/app/title/title.component.ts
+++ Ganhando Produtividade com Angular/LifeCycle Hooks Angular/lyfe-cycle/src/app/title/title.component.ts
@@ -1,14 +1,18 @@
- import { Component, OnInit } from '@angular/core';
+ import { Component, Input,  OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
- name: string = "Max"
+ @Input() name: string =''
+
   constructor() { }
+  ngOnChanges(): void {
+    console.log("OnChange")
+  }
 
   ngOnInit(): void {
 
