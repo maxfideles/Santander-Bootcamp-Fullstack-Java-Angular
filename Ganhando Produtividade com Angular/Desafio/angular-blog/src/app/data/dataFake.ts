596ce9bdb97c230002490eb2913b0b3017d00b16
maxfideles <maxfideles24@gmail.com>
2023-09-18 00:02:10 -0300

    Small-Card Links Updated

--- Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/data/dataFake.ts
+++ Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/data/dataFake.ts
@@ -10,5 +10,19 @@ export const dataFake = [
         "photo": "https://static.euronews.com/articles/stories/07/89/10/06/773x435_cmsv2_5ab0bdff-4d53-5214-9f7d-278df8301205-7891006.jpg",
         "title": "New iPhone 15 with better cameras, faster chips and USB-C chargers unveiled",
         "description":"Apple on Tuesday unveiled its next generation of iPhones - a line-up that will boast better cameras, faster processors, a new charging system and a price hike for the fanciest model. The showcase at Apple's headquarters in Cupertino, California, in the US comes as the company tries to reverse a mild slump that has seen its sales drop from last year in three consecutive quarters. The malaise is a key reason Apple's stock price has dipped by about 10 per cent since mid-July, dropping the company's market value below the $3 trillion (€2.7 trillion) threshold it reached for the first time earlier this summer."
+    },
+    {
+        "id":"3",
+        "photo": "https://www.cnet.com/a/img/resize/56d4bad5bea46a4f46dd4171ddca2269d3eebdfd/hub/2023/09/16/66da0526-2d64-4233-9aec-59ca8baddf46/ai-abstraction-getty.jpg?auto=webp&fit=crop&height=675&width=1200",
+        "title": "AI and You: Big Tech Says AI Regulation Needed, Microsoft Takes On Copyright Risks",
+        "description":`In a move that should surprise no one, tech leaders who gathered at closed-door meetings in Washington, DC, this week to discuss AI regulation with legislators and industry groups agreed on the need for laws governing generative AI technology. But they couldn't agree on how to approach those regulations. 
+
+        "The Democratic senator Chuck Schumer, who called the meeting 'historic,' said that attendees loosely endorsed the idea of regulations but that there was little consensus on what such rules would look like," The Guardian reported. "Schumer said he asked everyone in the room — including more than 60 senators, almost two dozen tech executives, advocates and skeptics — whether government should have a role in the oversight of artificial intelligence, and that 'every single person raised their hands, even though they had diverse views.'"`
+    },
+    {
+        "id":"4",
+        "photo":"https://www.ft.com/__origami/service/image/v2/images/raw/https%253A%252F%252Fcms-image-bucket-production-ap-northeast-1-a7d2.s3.ap-northeast-1.amazonaws.com%252Fimages%252F9%252F5%252F0%252F6%252F46536059-3-eng-GB%252FCropped-169470323320230914N%2520China%2520hypersonic%2520missile.JPG?width=700&fit=cover&gravity=faces&dpr=2&quality=medium&source=nar-cms",
+        "title": "China leads high-tech research in 80% of critical fields",
+        "description": "TOKYO -- China leads advanced technological research in 80% of critical fields including hypersonics and underwater drones, a report from an Australian think tank shows, as the country pulls ahead of the U.S., Europe and Japan through state-led investment."
     }
 ]
