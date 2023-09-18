44fcccaeb5f6c4f9f0717a5d28951616fbd50ec0
maxfideles <maxfideles24@gmail.com>
2023-09-17 23:51:00 -0300

    Routers and Pages Added

--- Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/data/dataFake.ts
+++ Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/data/dataFake.ts
@@ -1,9 +1,14 @@
 export const dataFake = [
     {
-        "id":1,
-        "photo":"asd",
-        "title": "asdad",
-        "description": "asdasd"
-
+        "id":"1",
+        "photo":"https://images.unsplash.com/photo-1516656690944-e76698b2aa38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRhcmslMjBjb2xvcnN8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+        "title": "Forget A New MacBook Pro, Apple Has Something Much Better",
+        "description": "Apple is expected to delay the launch of the new M3-powered MacBook Pro and MacBook Air. For months, a mid-October release has been on the cards and macOS fans were preparing to upgrade their laptops. With the laptops held back to 2024, what options are left for those needing to take the next step?"
+    },
+    {
+        "id":"2",
+        "photo": "https://static.euronews.com/articles/stories/07/89/10/06/773x435_cmsv2_5ab0bdff-4d53-5214-9f7d-278df8301205-7891006.jpg",
+        "title": "New iPhone 15 with better cameras, faster chips and USB-C chargers unveiled",
+        "description":"Apple on Tuesday unveiled its next generation of iPhones - a line-up that will boast better cameras, faster processors, a new charging system and a price hike for the fanciest model. The showcase at Apple's headquarters in Cupertino, California, in the US comes as the company tries to reverse a mild slump that has seen its sales drop from last year in three consecutive quarters. The malaise is a key reason Apple's stock price has dipped by about 10 per cent since mid-July, dropping the company's market value below the $3 trillion (€2.7 trillion) threshold it reached for the first time earlier this summer."
     }
 ]
--- Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/pages/content/content.component.ts
+++ Ganhando Produtividade com Angular/Desafio/angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -10,9 +11,11 @@ export class ContentComponent implements OnInit {
   @Input()
   imgCover: string ="https://www.ft.com/__origami/service/image/v2/images/raw/https%253A%252F%252Fcms-image-bucket-production-ap-northeast-1-a7d2.s3.ap-northeast-1.amazonaws.com%252Fimages%252F9%252F5%252F0%252F6%252F46536059-3-eng-GB%252FCropped-169470323320230914N%2520China%2520hypersonic%2520missile.JPG?width=700&fit=cover&gravity=faces&dpr=2&quality=medium&source=nar-cms"
   @Input()
-  contentTitle: string = "China asdadadad akdj"
+  contentTitle: string = ""
   @Input()
   contentDescription: string = "klasjdlkasjd China akljsdlakjhsdad"
+
+  private id: string | null= "0"
 
   constructor(private route: ActivatedRoute) { 
   }
@@ -20,9 +23,19 @@ export class ContentComponent implements OnInit {
   ngOnInit(): void {
 
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
-      )
+      this.id = value.get("id"))
 
+      this.setValuestoContent(this.id)
+
+  }
+
+  setValuestoContent (id:string|null){
+    const result = dataFake.filter(article => article.id == id)[0]
+  
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.imgCover = result.photo
+    
   }
 
 }
