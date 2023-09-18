d05444e045faf643d48dc222d2d50a9ecc4c113f
maxfideles <maxfideles24@gmail.com>
2023-09-18 18:05:54 -0300

    timerRedirector

--- Trabalhando com Rotas e Serviços no Angular/Trabalhando com Rotas no Angular/routes/src/app/pages/portfolio/card/card.component.ts
+++ Trabalhando com Rotas e Serviços no Angular/Trabalhando com Rotas no Angular/routes/src/app/pages/portfolio/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -9,14 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 export class CardComponent implements OnInit {
 
   private id:string | null=""
-  constructor(private route: ActivatedRoute) { 
- 
-  }
+  constructor(
+    private route: ActivatedRoute, 
+    private timerRedirector:Router
+    ) { }
 
   ngOnInit(): void {
+    //Forcing the routing to a specific component
+    setInterval(() =>  this.timerRedirector.navigate([''])  ,10000)
+
   //Getting the parameters : http://localhost:4200/portfolio/{1}
     this.route.paramMap.subscribe(value =>console.log(value.get("id")))
-
+  //Getting Querys http://localhost:4200/portfolio/2{?name=Max&surname=Test}
     this.route.queryParams.subscribe(value => console.log(value))
   
   }
