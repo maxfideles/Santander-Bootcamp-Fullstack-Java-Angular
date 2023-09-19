1be5d86323ab84ba69542325020fe5888deebdcf
maxfideles <maxfideles24@gmail.com>
2023-09-19 16:36:22 -0300

    First Commit

---
+++ Trabalhando com Rotas e Serviços no Angular/Recriando a Interface da PlayStation Store com Angular/store/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardsComponent } from './components/cards/cards.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    CardsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
