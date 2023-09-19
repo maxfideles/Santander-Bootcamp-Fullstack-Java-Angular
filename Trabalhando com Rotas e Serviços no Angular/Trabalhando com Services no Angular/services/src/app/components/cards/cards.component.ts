2ba44b03345175c8bcc5fa5a624b8f87281becef
maxfideles <maxfideles24@gmail.com>
2023-09-18 22:46:27 -0300

    Observable Added

--- Trabalhando com Rotas e Serviços no Angular/Trabalhando com Services no Angular/services/src/app/components/cards/cards.component.ts
+++ Trabalhando com Rotas e Serviços no Angular/Trabalhando com Services no Angular/services/src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-cards',
@@ -8,15 +9,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardsComponent implements OnInit {
 
   @Input ()
-  pokeName:string = "Bulbasaura"
+  pokeName:string = "Bulbasaur"
   @Input ()
   pokeImg:string = "https://m.media-amazon.com/images/I/51afsafzcjL._AC_UF894,1000_QL80_.jpg"
   @Input()
   pokeTyps:string[]=["Grass","Water"]
 
-  constructor() { }
+  constructor(private service: PokemonService) { }
 
   ngOnInit(): void {
+
+    this.service.getPokemon("ditto").subscribe(
+      {
+        next: (response) => console.log(response),
+        error: (erro) => console.log(erro)
+      }
+
+
+    )
+
   }
 
 }
---
+++ Trabalhando com Rotas e Serviços no Angular/Trabalhando com Services no Angular/services/src/app/models/pokemonData.ts
@@ -0,0 +1,4 @@
+export type PokemonData = {
+    name:"",
+    id:""
+}
